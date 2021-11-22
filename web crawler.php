7bf5613eaf12cf76060e707de394c712ee043d48
Vinod <vinodawati7585@gmail.com>
2021-11-22 19:53:07 +0530

    initial commit

---
+++ web crawler.php
@@ -0,0 +1,35 @@
+
+<?php
+
+
+getdeta("https://www.google.com",5);
+
+function getdeta($url,$depth) {
+   $j=1;
+    while ($j<=$depth) {
+         $doc =new DOMDocument();
+         $doc->loadHTML(file_get_contents($url));
+         $linklist =$doc->getElementsByTagName("a");
+         $links=array();
+         $index=0;
+
+    foreach ($linklist as $link) {
+         $links[$index] =$link->getAttribute("href");
+         echo "<br>";
+
+    if (preg_match("/photo/", $links[$index])){
+	     echo "the url images == $links[$index] = depth is".$j;
+         }
+
+    else {
+	     echo "the url Links == $links[$index]= depth is".$j;
+         }
+         $index++;
+         echo "<br>";
+    }   
+         $url=$links[$j-1];
+         
+         $j++;
+    }   
+}s
+  ?>
